0b99b64182010305a988a67bdf16b30789b9ecc1
Matthew Woodring <34324986+TheHikingCoder@users.noreply.github.com>
2023-03-24 09:46:22 -0500

    Update login.php

--- Assignment03/login.php
+++ Assignment03/login.php
@@ -5,15 +5,14 @@
 	// Gets email and password from the 'login.html' form
 	$email = $_POST['email'];
 	$password = $_POST['password'];
-
-	// Hardcoded values; will replace with SQL query in Assignment 04
-	$hardcodedEmail = 't@t.com';
-	$hardcodedPassword = '123';
-
-	// This triggers if the attempted login matches a registered user's credentials
-	if ($email == $hardcodedEmail && $password == $hardcodedPassword)
+	
+	// Calls 'login' function to validate entered credentials
+	$result = login($email, $password);
+	
+	// Triggered if '$result' is true
+	if ($result)
 	{
-		
+	
 		// Create session
 		$_SESSION['email'] = $email;
 		
@@ -22,16 +21,41 @@
 		header("Location: fuelQuoteForm.html");
 		exit();
 		
-	} 
-
-	// This triggers if the attempted login does not match a registered user's credentials
+	}
+	
+	// Triggered if '$result' is false
 	else
 	{
-
+		
 		// Displays an error message
 		header("Location: login.php?error=1");
 		exit();
 		
+	}
+	
+	function login($email, $password)
+	{
+		
+		// Hardcoded values; will replace with SQL query in Assignment 04
+		$hardcodedEmail = 't@t.com';
+		$hardcodedPassword = '123';
+		
+		// This triggers if the attempted login matches a registered user's credentials
+		if ($email == $hardcodedEmail && $password == $hardcodedPassword)
+		{
+
+			return true;
+		
+		} 
+
+		// This triggers if the attempted login does not match a registered user's credentials
+		else
+		{
+
+			return false;
+		
+		}
+	
 	}
 	
 ?>
